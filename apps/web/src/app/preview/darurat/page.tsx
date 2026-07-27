b64c985db2f071d9b198734d02049bea0987afd0
Victor Tremor <vt.xillaries@gmail.com>
2026-07-27 22:32:20 +0800

    pans

---
+++ apps/web/src/app/preview/darurat/page.tsx
@@ -0,0 +1,10 @@
+import "./v17-darurat.css";
+import { DaruratPanel } from "@/widgets/darurat-panel";
+
+export const metadata = {
+  title: "Mode Darurat | Tinyverse",
+};
+
+export default function DaruratPage() {
+  return <DaruratPanel />;
+}
