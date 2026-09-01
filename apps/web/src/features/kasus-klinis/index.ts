9660bb45ab192b520304282613eae690d15eadc1
Victor Tremor <vt.xillaries@gmail.com>
2026-09-01 16:07:10 +0800

    update menu baru

--- apps/web/src/features/kasus-klinis/index.ts
+++ apps/web/src/features/kasus-klinis/index.ts
@@ -1,20 +1,9 @@
-import { kasusDehidrasi } from "./dehidrasi";
-import { kasusIkterus } from "./ikterus";
-import { kasusAsmatikus } from "./asmatikus";
-import { kasusFttGizi } from "./ftt-gizi";
-import { kasusKejangDemam } from "./kejang-demam";
-import type { Kasus } from "../types";
-
-export const SEMUA_KASUS: ReadonlyArray<Kasus> = [
+export {
+  SEMUA_KASUS,
+  getKasusById,
   kasusDehidrasi,
   kasusIkterus,
   kasusAsmatikus,
   kasusFttGizi,
   kasusKejangDemam,
-];
-
-export function getKasusById(id: string): Kasus | undefined {
-  return SEMUA_KASUS.find((k) => k.id === id);
-}
-
-export { kasusDehidrasi, kasusIkterus, kasusAsmatikus, kasusFttGizi, kasusKejangDemam };
+} from "./data";
